9ffe95b682863daa4e33892093b853405f7bf6cc
Stephan Dinkelaar <sdinkelaar@roc-dev.com>
2015-10-14 13:14:44 +0200

    alleess van bierhier prject

---
+++ src/nl/awesomejustice/AwesomeJusticeServlet.java
@@ -0,0 +1,12 @@
+package nl.awesomejustice;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class AwesomeJusticeServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
